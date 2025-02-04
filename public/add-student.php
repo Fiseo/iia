198def73dbf34436d5527ca6777eaba1dd4e29a2
Vigile Martinier <virgile.martinier6002@gmail.com>
2025-02-04 09:54:52 +0100

    Possibilité d'ajout d'étudiant dans la bdd

---
+++ public/add-student.php
@@ -0,0 +1,63 @@
+<?php
+require("../paspublic/connect.php");
+?>
+<html>
+<head>
+    <title>Gestion de l'IIA</title>
+</head>
+<body>
+<form action="" method="post" enctype="multipart/form-data">
+    <h1>Ajout d'un étudiant</h1>
+    <div>
+        <label for="nom">Nom :</label>
+        <input id="nom" name="nom" type="text"/>
+    </div>
+    <div>
+        <label for="prenom">Prénom :</label>
+        <input id="prenom" name="prenom" type="text"/>
+    </div>
+    <?php
+    $promotions = getPromotion();
+    foreach ($promotions as $promotion) {
+        echo "<input id=\"".$promotion[0]."\"type=\"radio\" name=\"promo\" value=\"".$promotion[1]."\"/><label for=\"".$promotion[0]."\">".$promotion[1]."</label>";
+
+    }
+    ?>
+    <div>
+        <input type="submit" value="Envoyer le formulaire">
+    </div>
+</form>
+<a href="index.php">Retour au choix de promotion</a><br/>
+<a href="add-promo.php">Rajouter une promotion</a>
+</body>
+</html>
+
+<?php
+if (isset($_POST['promo'])){
+    $sql = $pdo ->prepare("INSERT INTO Etudiant(Nom,Prenom,IdentifiantPromotions) VALUES (:nom, :prenom, :promo);");
+}else{
+    $sql = $pdo ->prepare("INSERT INTO Etudiant(Nom,Prenom) VALUES (:nom, :prenom);");
+}
+
+if (isset($_POST['nom']) && isset($_POST['prenom']) && $_POST['nom'] != "" && $_POST['prenom'] != "") {
+    if (isset($_POST['promo'])){
+        foreach ($promotions as $promotion){
+            if ($promotion[1] == $_POST['promo']) {
+                $id = $promotion[0];
+                break;
+            }
+        }
+    }
+
+    $sql->bindParam(":nom", $_POST['nom']);
+    $sql->bindParam(":prenom", $_POST['prenom']);
+    if (isset($_POST['promo'])){
+        $sql->bindParam(":promo", $id);
+    }
+
+    $sql->execute();
+
+} else {
+    echo "veuillez remplir tous les champs";
+}
+?>
